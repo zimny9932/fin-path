9905afeeeed85349d358a0cd2b8098c3603417e5
Wojciech Zimoń <wojciechzimon93@gmail.com>
2026-01-27 22:39:32 +0100

    e2e test

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'integration' => true],
 ];
